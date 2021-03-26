51378bde0d3480595276b22bca93b40455eabcbc
Mathieu Méa <mathieu.mea@gmail.com>
2021-03-26 11:11:49 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/ca_edmonton_ets_train/EdmontonETSTrainAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_edmonton_ets_train/EdmontonETSTrainAgencyTools.java
@@ -2,22 +2,16 @@ package org.mtransit.parser.ca_edmonton_ets_train;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CharUtils;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.StringUtils;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GIDs;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
-import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
 import java.util.regex.Pattern;
 
 import static org.mtransit.parser.Constants.EMPTY;
@@ -58,6 +52,12 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 		return gRoute.getRouteType() != MAgency.ROUTE_TYPE_LIGHT_RAIL; // declared as light rail but we classify it as a train (not on the road)
 	}
 
+	@Nullable
+	@Override
+	public String getRouteShortName(@NotNull GRoute gRoute) {
+		return super.getRouteShortName(gRoute); // used by real-time API (fall back on route ID)
+	}
+
 	@Override
 	public long getRouteId(@NotNull GRoute gRoute) {
 		if (RSN_CAPITAL_LINE.equalsIgnoreCase(gRoute.getRouteShortName())) {
@@ -65,7 +65,7 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 		} else if (RSN_METRO_LINE.equalsIgnoreCase(gRoute.getRouteShortName())) {
 			return 502L;
 		}
-		return Long.parseLong(gRoute.getRouteShortName()); // using route short name as route ID
+		throw new MTLog.Fatal("Unexpected route ID for %s!", gRoute); // used by real-time API (fall back from route short name)
 	}
 
 	private static final String RSN_CAPITAL_LINE = "Capital";
@@ -75,10 +75,9 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 
 	@NotNull
 	@Override
-	public String getRouteLongName(@NotNull GRoute gRoute) {
-		String gRouteLongName = gRoute.getRouteLongNameOrDefault();
-		gRouteLongName = CLEAN_STARTS_LRT.matcher(gRouteLongName).replaceAll(EMPTY);
-		return CleanUtils.cleanLabel(gRouteLongName);
+	public String cleanRouteLongName(@NotNull String routeLongName) {
+		routeLongName = CLEAN_STARTS_LRT.matcher(routeLongName).replaceAll(EMPTY);
+		return CleanUtils.cleanLabel(routeLongName);
 	}
 
 	private static final String AGENCY_COLOR_BLUE = "2D3092"; // BLUE (from Wikipedia SVG)
@@ -98,7 +97,7 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 	@Override
 	public String getRouteColor(@NotNull GRoute gRoute) {
 		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
-			if (Utils.isDigitsOnly(gRoute.getRouteShortName())) {
+			if (CharUtils.isDigitsOnly(gRoute.getRouteShortName())) {
 				int rsn = Integer.parseInt(gRoute.getRouteShortName());
 				switch (rsn) {
 				case 501:
@@ -122,9 +121,8 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 		return true;
 	}
 
-	private static final String NAIT = "NAIT";
 	private static final Pattern N_A_I_T_ = Pattern.compile("(n a i t)", Pattern.CASE_INSENSITIVE);
-	private static final String N_A_I_T_REPLACEMENT = NAIT;
+	private static final String N_A_I_T_REPLACEMENT = "NAIT";
 
 	@NotNull
 	@Override
@@ -137,9 +135,8 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 
 	private static final Pattern ENDS_WITH_STATION = Pattern.compile("([\\s]*station[\\s]*$)", Pattern.CASE_INSENSITIVE);
 
-	private static final String EDMONTON = "Edm";
-	private static final Pattern EDMONTON_ = Pattern.compile("((^|\\W)(edmonton)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String EDMONTON_REPLACEMENT = "$2" + EDMONTON + "$4";
+	private static final Pattern EDMONTON_ = CleanUtils.cleanWord("edmonton");
+	private static final String EDMONTON_REPLACEMENT = CleanUtils.cleanWordsReplacement("Edm");
 
 	@NotNull
 	@Override
@@ -149,5 +146,14 @@ public class EdmontonETSTrainAgencyTools extends DefaultAgencyTools {
 		gStopName = CleanUtils.cleanStreetTypes(gStopName);
 		gStopName = CleanUtils.cleanNumbers(gStopName);
 		return CleanUtils.cleanLabel(gStopName);
+	}
+
+	@NotNull
+	@Override
+	public String getStopCode(@NotNull GStop gStop) {
+		if (!CharUtils.isDigitsOnly(gStop.getStopCode())) {
+			throw new MTLog.Fatal("Unexpected stop code %s!", gStop);
+		}
+		return super.getStopCode(gStop); // used by real-time provider
 	}
 }
